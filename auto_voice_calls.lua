77f4459da05f6507f8162bf9c956ec6129c09677
Alexx <eroszansan@gmail.com>
2024-03-24 19:48:33 +0000

    Refactor require statements in auto_engi.lua and auto_voice_calls.lua

--- auto_voice_calls.lua
+++ auto_voice_calls.lua
@@ -15,24 +15,21 @@ print("Loading " .. config.scriptName .. "...")
 
 -- Load the required libraries and throw an error if they are not found and exit the script
 local localappdata = os.getenv("LOCALAPPDATA")
-local al = require(localappdata .. "\\LUAS\\alex_lib.lua")
+local al = require(localappdata .. "\\LUAS\\alex_lib")
 if not al then
     error("alex_lib not found")
 end
 
 ---Check if there is a spy near and voice out the message
----@param players table
-local function spy_call(players)
+local function spy_call()
     -- Loop through all the players
     local time_between_calls = 12
-    local spy_distance = 200
-    for _, player in pairs(players) do
+    local spy_distance = 300
+    for _, Player in pairs(Players) do
         -- Check if the player is an enemy spy and is near.
-        -- Make spy call only once every "time_between_calls" seconds
-        if player:GetTeamNumber() ~= Me:GetTeamNumber() and player:GetPropInt("m_iClass") == al.TF2_CLASSES.SPY and al.distance(Me, player) < spy_distance and globals.CurTime() - Last_spy_call_time > time_between_calls and not Call_made then
+        if Player:GetTeamNumber() ~= Me:GetTeamNumber() and Player:GetPropInt("m_iClass") == al.TF2_CLASSES.SPY and al.distance(Me, Player) < spy_distance and globals.CurTime() - Last_call_time > time_between_calls and not Call_made then
             client.Command("voicemenu 1 1", true)
-            Last_spy_call_time = globals.CurTime()
-            Last_call_type = "Spy"
+            Last_call_time = globals.CurTime()
             Call_made = true
             al.printifv("Spy")
         end
@@ -40,35 +37,35 @@ local function spy_call(players)
 end
 
 ---Make a call to the medic if the player is low on health
-
 local function medic_call()
     local time_between_calls = 6
-    if Me:GetHealth() < Me:GetMaxHealth() and globals.CurTime() - Last_medic_call_time > time_between_calls and not Call_made then
+    if Me:GetHealth() < Me:GetMaxHealth() and globals.CurTime() - Last_call_time > time_between_calls and not Call_made then
         client.Command("voicemenu 0 0", true)
         al.announce(config.scriptName, "Calling for a Medic")
-        Last_medic_call_time = globals.CurTime()
-        Last_call_type = "Medic"
+        Last_call_time = globals.CurTime()
         Call_made = true
         al.printifv("Calling for a Medic")
     end
 end
 
 -- Define the function to voice out the message
-Last_medic_call_time = 0
-Last_spy_call_time = 0
-Overall_last_call_time = 0
+Last_call_time = 0
 Call_made = false
+
 -- Make a call once every second
 local function onCreateMove()
+    -- Reset flags
+    Call_made = false
+
     -- Get the local player
     local Me = entities.GetLocalPlayer()
     if not Me then return end
 
     -- Get all the players
-    local players = al.getPlayers()
+    Players = al.getPlayers()
 
     -- Spy call
-    spy_call(players)
+    spy_call()
 
     -- Medic call
     medic_call()
